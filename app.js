ae7f3117ec105cd06f3edd973c6d54df185a3a9e
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-01 11:24:51 +0200

    fix bug

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 // Selectors
-const calories = document.querySelector(".calories-result");
-const mealInput = document.getElementById("meal-input");
-const calorieInput = document.getElementById("calorie-input");
-const addMealButton = document.querySelector(".add-meal-button");
+const totalCalories = document.querySelector(".calories-result");
