911c449fb1ab4212a6425022f25a9f19674e46c7
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-01 11:27:41 +0200

    select all required elements

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 // Selectors
 const totalCalories = document.querySelector(".calories-result");
+const calorieInput = document.querySelector("#calorie-input");
+const mealInput = document.querySelector("#meal-input");
+const addMealButton = document.querySelector(".add-meal-btn");
